5531fe4af9c01b26f6cd8bea1bb38329deace2f5
Sebastian-Simek <sebastiansimek09@gmail.com>
2023-06-30 11:31:46 -0700

    ft. broken jest for now

---
+++ src/components/__tests__/DummyComponent.test.js
@@ -0,0 +1,9 @@
+import React from "react";
+import { render, test, expect } from "@testing-library/react";
+import DummyComponent from "../DummyComponent";
+
+test('should render "DummyComponent" in a div', () => {
+  const { getByText } = render(<DummyComponent />);
+  const dummyText = getByText("DummyComponent");
+  expect(dummyText).toBeInTheDocument();
+});
