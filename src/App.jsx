85ca9fd10a3b6cd3c1767262123fa9372db9b118
David <Rigby335@hotmail.com>
2023-07-19 10:39:32 -0700

    ft. got API data on front end

--- src/App.jsx
+++ src/App.jsx
@@ -3,27 +3,24 @@ import { getMovieBySearch } from "./utlils/tmdb-utils";
 
 function App() {
   const [search, setSearch] = useState("");
-  const [staticSearch, setStaticSearch] = useState("");
   const [searchResult, setSearchResult] = useState({});
 
   async function handleSubmit() {
-    setStaticSearch(search);
-    const result = await getMovieBySearch(staticSearch);
+    const result = await getMovieBySearch(search);
     setSearchResult(result);
     console.log("searchResult", searchResult);
   }
   return (
     <>
       <h1>Hello from TMDB-Search-Display</h1>
-      <form onSubmit={() => handleSubmit()}>
-        <input
-          placeholder="Search"
-          type="text"
-          onChange={(e) => setSearch(e.target.value)}
-        />
-        <button type="submit">Submit</button>
-      </form>
-      <p>{searchResult.results}</p>
+      <input
+        placeholder="Search"
+        type="text"
+        onChange={(e) => setSearch(e.target.value)}
+      />
+      <button type="submit" onClick={() => handleSubmit()}>
+        Submit
+      </button>
     </>
   );
 }
