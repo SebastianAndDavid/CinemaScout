f21b6a81ce240f8508f30a148312270b7b1d4d15
David <Rigby335@hotmail.com>
2023-08-24 09:53:28 -0700

    ft. moved click event to the x-button

--- src/components/DetailCard.jsx
+++ src/components/DetailCard.jsx
@@ -8,7 +8,7 @@ export default function DetailCard({
   const BASE_POSTER_URL = "https://image.tmdb.org/t/p/w500";
 
   return (
-    <div className="detail-card" onClick={() => handleDetailCardClick()}>
+    <div className="detail-card">
       <div className="carrot-container-1">
         <a
           className="carrot-1"
@@ -30,7 +30,9 @@ export default function DetailCard({
         ></a>
       </div>
       <div className="x-container">
-        <div className="x-button">&times;</div>
+        <div className="x-button" onClick={() => handleDetailCardClick()}>
+          &times;
+        </div>
       </div>
     </div>
   );
