862376d7dc20c94d0f597f04a2a59b293a0fbb96
David <Rigby335@hotmail.com>
2023-09-06 10:20:36 -0700

    ft. setting credit state on carrot click

--- src/App.jsx
+++ src/App.jsx
@@ -65,7 +65,11 @@ function App() {
         currentMovieIndex === searchResult.length - 1
           ? await getDetailsById(searchResult[0].id)
           : await getDetailsById(searchResult[currentMovieIndex + 1].id);
-
+      const creditResult =
+        currentMovieIndex === searchResult.length - 1
+          ? await getCreditsById(searchResult[0].id)
+          : await getCreditsById(searchResult[currentMovieIndex + 1].id);
+      setCredits(creditResult);
       setMovieDetails(results);
       setDidClickCarrot(true);
     } else if (carrotDirection === "left") {
