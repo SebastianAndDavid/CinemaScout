2cd3d5129432ea9b684b9b268ec5669daf88f5e3
David <Rigby335@hotmail.com>
2023-10-02 01:31:26 -0700

    ft. tv show detail component scaffolding, now has poster

--- src/components/TVShowDetailCard.jsx
+++ src/components/TVShowDetailCard.jsx
@@ -1,9 +1,27 @@
 import PropTypes from "prop-types";
 
 export default function TVShowDetailCard({ TVShowDetails }) {
+  const BASE_POSTER_URL = "https://image.tmdb.org/t/p/w500";
   console.log("TVShowDetails", TVShowDetails);
 
-  return <div>TVShowDetailCard</div>;
+  return (
+    <div className="detail-card">
+      <div className="carrot-container-1"></div>
+      <div className="detail-image">
+        {TVShowDetails.poster_path ? (
+          <img
+            style={{ width: "240px", height: "360px" }}
+            src={BASE_POSTER_URL + TVShowDetails.poster_path}
+          />
+        ) : (
+          <img style={{ width: "240px", height: "360px" }} src="niccage.jpg" />
+        )}
+      </div>
+      <div className="detail-text"></div>
+      <div className="carrot-container-2"></div>
+      <div className="x-container"></div>
+    </div>
+  );
 }
 TVShowDetailCard.propTypes = {
   TVShowDetails: PropTypes.object.isRequired,
